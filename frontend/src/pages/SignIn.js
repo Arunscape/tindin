2446cc9cf5f96846ec8afd5130c0ba8ce92376ed
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-01-18 19:12:32 -0700

    centered text with garros

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
 import styled from 'styled-components'
-const centering=styled.div`
-text-align:center`
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const Centering = styled.div`
+text-align: center;
+margin: 2rem;
+`;
+
 
 export default () => {
     const history = useHistory();
 
     return <>
-        <centering>Welcome</centering>
+        <CssBaseline />
+        <Centering>Welcome</Centering>
         <button onClick={() => history.push("/signin-email")}>Sign in</button>
     </>
 
