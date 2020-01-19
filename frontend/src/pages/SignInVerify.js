a2ce8de456e13b451793ae6b7e18c3bab8a61972
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-01-19 04:48:47 -0700

    we have signin

--- frontend/src/pages/SignInVerify.js
+++ frontend/src/pages/SignInVerify.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import useGlobalState from '../useGlobalState';
 
 import CONFIG from '../config';
@@ -18,86 +18,135 @@ const useStyles = makeStyles(theme => ({
 export default () => {
     const classes = useStyles();
 
-    const [valid, setValid] = useState('unknown');
     const { user, setUser, setLoggedIn } = useGlobalState();
 
-    const [tempToken, setTempToken] = useState(null);
+    // const [tempToken, setTempToken] = useState(null);
+    let timer;
+
+    const history = useHistory();
 
 
     useEffect(() => {
-        const getToken = async () => await fetch(CONFIG.API + '/signin', {
+        const getTempToken = () => fetch(CONFIG.API + '/signin', {
             method: 'post',
-            body: { email: user.email },
+            body: JSON.stringify({ email: user.email }),
+            headers: {
+                "Content-Type": "application/json"
+            },
 
-        }).catch(e => {
-            console.log("HELLO" + e);
-            return null;
+        }).then(res => res.json())
+            .then(data => {
+                console.log('TEMP TOKEN');
+                console.log(data);
+                if (data !== null) {
+                    timer = setInterval(() => {
+                        console.log('This will run after 1 second!')
+                        console.log(data)
+                        if (data == null) {
+                            return;
+                        }
 
-        })
+                        const upgrade = () => fetch(CONFIG.API + '/upgrade', {
+                            method: 'post',
+                            body: JSON.stringify({ tok: data }),
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
 
-        const tok = getToken();
+                        }).then(res => res.json()).then(d => {
+                            console.log("ACTUAL TOKEN");
+                            console.log(d)
 
-        if (tok !== null) {
-            setTempToken(tok);
-        }
+                            if (d !== null) {
+                                setUser({ ...user, tok: d });
+                                console.log("USER")
+                                console.log(user)
+                                localStorage.setItem('userToken', d)
+                                history.push("/");
 
-    }, []);
+                            }
+                        }).catch(e => {
+                            console.log("HELLO" + e);
+                            return null;
+
+                        })
+
+                        const res = upgrade();
 
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            console.log('This will run after 1 second!')
-            if (user.tok == null) {
-                return;
-            }
 
-            const upgrade = async () => await fetch(CONFIG.API + '/upgrade', {
-                method: 'post',
-                body: { token: user.token },
+                    }, 1000);
 
+                }
             }).catch(e => {
-                console.log("HELLO" + e);
+                console.log("error getting temp token" + e);
                 return null;
 
             })
 
-            const res = upgrade();
 
-            if (res.status === 400) {
-                return;
-            }
 
-            if (res.status === 200) {
-                setUser({ ...user, tok: res.data });
+        getTempToken();
+        // const tok = getTempToken();
+        // console.log("TEMP TOKEN: ");
+        // console.log(tempToken);
+        // console.log(tok)
 
-            }
+        // if (tok !== null) {
+        //     setTempToken(tok);
+        // }
 
-        }, 1000);
         return () => clearInterval(timer);
+
     }, []);
 
-    useEffect(() => {
 
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         console.log('This will run after 1 second!')
+    //         console.log(tempToken)
+    //         if (tempToken == null) {
+    //             return;
+    //         }
 
+    //         const upgrade = () => fetch(CONFIG.API + '/upgrade', {
+    //             method: 'post',
+    //             body: JSON.stringify({ tok: tempToken }),
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             },
 
-        // make post request
+    //         }).then(res => res.json()).then(data => {
+    //             console.log("ACTUAL TOKEN");
+    //             console.log(data);
+    //         }).catch(e => {
+    //             console.log("HELLO" + e);
+    //             return null;
 
-        //if response = 4XX
-        setValid('needssignup');
+    //         })
 
+    //         const res = upgrade();
 
-        //if response = 2XX
-        setValid('loggedin');
-        setLoggedIn(true);
+    //         if (res.status === 400) {
+    //             return;
+    //         }
 
-    }, [setLoggedIn, setValid])
+    //         if (res.status === 200) {
+    //             setUser({ ...user, tok: res.data });
+
+    //         }
+
+    //     }, 1000);
+    //     return () => clearInterval(timer);
+    // }, []);
+
 
     return (
         <div className={classes.root}>
             <div>Verifying...</div>
             <LinearProgress />
-            {valid === 'loggedin' && <Redirect to="/" />}
-            {valid === 'needssignup' && <Redirect to="/signup" />}
+            {/* {valid === 'loggedin' && <Redirect to="/" />}
+            {valid === 'needssignup' && <Redirect to="/signup" />} */}
         </div>
     );
 }
