8f34927840e68eb93c638c61b63aff0f76872578
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-01-18 20:40:37 -0700

    stuff

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import useGlobalState from '../useGlobalState';
 
+import styled from 'styled-components'
+
+const SwipeArea = styled.div`
+    width: 100%;
+    height: 100%;
+    `;
+
 export default () => {
 
+    const [touchStart, setTouchStart] = useState(null);
+    const [touchEnd, setTouchEnd] = useState(null);
+
+    const handleTouchStart = (event) => setTouchStart(event);
+    const handleTouchEnd = (event) => setTouchEnd(event);
+
+    useEffect(() => {
+        window.addEventListener('touchstart', handleTouchStart);
+        window.addEventListener('touchend', handleTouchEnd);
+
+        return () => {
+            window.removeEventListener('touchstart', handleTouchStart);
+            window.removeEventListener('touchend', handleTouchEnd);
+        };
+    }, []);
+
     const { loggedIn } = useGlobalState();
-    return <>
-        {loggedIn ? <div>You're logged in</div> : <Redirect to="/signin" />}
-    </>
+    // return <>
+    //     {loggedIn ? <div>You're logged in</div> : <Redirect to="/signin" />}
+    // </>
+
+    return <SwipeArea>
+        <div>Hello</div>
+        <div>Touchstart</div>
+        <div>{JSON.stringify(touchStart)}</div>
+        <div>Touchend</div>
+        <div>{JSON.stringify(touchEnd)}</div>
+    </SwipeArea>
 
 }
