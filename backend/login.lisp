2635fae9dd34809bab133d8ed7730d24ac61b814
Peter Elliott <pelliott@ualberta.ca>
2020-01-19 04:10:57 -0700

    made login return a json object

--- backend/login.lisp
+++ backend/login.lisp
@@ -50,8 +50,10 @@
                 (concatenate 'string "Click the link to verify your email"
                              (format nil "~%http://~A/api/validate/~A~%"
                                      *domain* slug)))
-    (list 200 nil (list (create-token (db:get-userid-by-email email) email nil)))))
-
+    `(200
+      (:content-type "application/json")
+      (,(json:encode-json-to-string
+         (create-token (db:get-userid-by-email email) email nil))))))
 
 (defun upgrade-or-error (tok)
   (if (and (is-valid tok) (db:has-current-validation (is-valid tok)))
