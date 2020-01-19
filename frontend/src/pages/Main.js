8c2e68527e8ae8674e4fb3ea7a1a1adcdc5b048f
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-01-18 23:14:15 -0700

    WHY THE FUCK IS IT NULL?

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -14,8 +14,10 @@ export default () => {
     const [touchStart, setTouchStart] = useState(null);
     const [touchEnd, setTouchEnd] = useState(null);
 
+    const [angle, setAngle] = useState(null);
+
     const handleTouchStart = (e) => {
-        console.log(e)
+        // console.log(e)
         e.preventDefault();
         const x = e.touches[0].screenX;
         const y = e.touches[0].screenY;
@@ -23,20 +25,33 @@ export default () => {
             x,
             y,
         });
+        console.log("TOUCHSTART: ", x, y);
+        if (touchStart == null) {
+            console.log("WHY THE FUCK IS IT NULL?")
+        }
     }
     const handleTouchEnd = (e) => {
-        console.log(e)
+        // console.log(e)
         e.preventDefault();
         const x = e.changedTouches[0].screenX;
         const y = e.changedTouches[0].screenY;
         setTouchEnd({
             x,
             y,
-            diffX: touchStart ? touchStart.x - x : null,
-            diffY: touchStart ? touchStart.y - y : null,
-            angle: touchStart ? Math.atan2(touchStart.y - y, touchStart.x - x) : null,
         });
+        console.log("TOUCHEND: ", JSON.stringify(touchEnd));
     }
+
+    useEffect(() => {
+
+        if (touchStart == null || touchEnd == null) {
+            return;
+        }
+
+        setAngle(Math.atan2(touchEnd.y - touchStart.y, touchEnd.x - touchStart.x));
+
+        console.log("ANGLE: " + angle)
+    }, touchStart, touchEnd)
 
     useEffect(() => {
         window.addEventListener('touchstart', handleTouchStart);
@@ -59,6 +74,8 @@ export default () => {
         <div>{JSON.stringify(touchStart)}</div>
         <div>Touchend</div>
         <div>{JSON.stringify(touchEnd)}</div>
+        <div>Angle</div>
+        <div>{angle}</div>
     </SwipeArea>
 
 }
