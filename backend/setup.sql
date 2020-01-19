89b3e5fe2c22e542a5dd552b7572d451dfc63c8c
Jacob Reckhard <jacobreckhard@gmail.com>
2020-01-19 11:44:56 -0700

    300 char

--- backend/setup.sql
+++ backend/setup.sql
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS swipes (
 
 INSERT INTO users (uid, uname, bio, email) VALUES
   (1, "Jacob", "Coolest", "jacob@reckhard.ca"),
-  (2, "Peter", "Coolish", "pelliott@ualberta.ca"),
+  (2, "Peter", "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with", "pelliott@ualberta.ca"),
   (3, "Arun", "I use Arch Linux, btw", "awoosare@ualberta.ca"),
   (4, "Christiana", "Birds are dinosaurs!", "garros@ualberta.ca");
 
