0f015a2fc99c45ee87a718c73edb9280a2f3e23b
Christiana Garros <christianawgarros@gmail.com>
2020-01-19 10:15:07 -0700

    styled the verifying page

--- frontend/src/pages/SignInVerify.js
+++ frontend/src/pages/SignInVerify.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { useHistory } from 'react-router-dom';
 import useGlobalState from '../useGlobalState';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import styled from 'styled-components'
 
 import CONFIG from '../config';
 
@@ -12,8 +14,23 @@ const useStyles = makeStyles(theme => ({
         '& > * + *': {
             marginTop: theme.spacing(2),
         },
+        backgroundImage: 'linear-gradient(#EE357B, #2768B3)',
+        height: '100vh'
+        
     },
 }));
+
+const Frame = styled.div`
+align-items: center;
+justify-content: center;
+display: flex;
+flex-flow: column nowrap;`
+
+const Text = styled.div`
+padding 1rem;
+font: bold italic 7vw Open Sans;
+font-size: 7vw;
+color: white;`
 
 export default () => {
     const classes = useStyles();
@@ -98,11 +115,12 @@ export default () => {
     }, []);
 
     return (
-        <div className={classes.root}>
-            <div>Verifying...</div>
-            <LinearProgress />
+        <Frame className={classes.root}>
+            <Text>Verifying...</Text>
+
+        <CircularProgress variant="indeterminate" style={{color: 'white'}}/>
             {/* {valid === 'loggedin' && <Redirect to="/" />}
             {valid === 'needssignup' && <Redirect to="/signup" />} */}
-        </div>
+        </Frame>
     );
 }
