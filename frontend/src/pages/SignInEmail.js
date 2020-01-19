5e3a997b5496508d6dbd37d193942f1a32fce8a4
Christiana Garros <christianawgarros@gmail.com>
2020-01-19 07:05:15 -0700

    added visuals to SignInEmail

--- frontend/src/pages/SignInEmail.js
+++ frontend/src/pages/SignInEmail.js
@@ -1,21 +1,55 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import { useHistory } from 'react-router';
 import useGlobalState from '../useGlobalState';
-import CONFIG from '../config'
+import CONFIG from '../config';
+import styled from 'styled-components';
+import Heart from '../assets/altheart.svg';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: 200,
-        },
-    },
-}));
+const Background = styled.div`
+height: 60vh;
+
+display: flex;
+flex-flow: column nowrap;
+align-items: center;
+justify-content: space-around;
+background-image: linear-gradient(#EE357B , #2768B3);
+height: 100vh;`
+const Title = styled.div`
+padding 1rem;
+font: bold italic 7vw Open Sans;
+font-size: 7vw;
+color: white;`
+const Text = styled.input`
+border: none;
+background-color: #FFFFFF00;
+color: white;
+font: bold 5vw Open Sans;
+border-bottom: 0.2rem solid white;`
+const Image = styled.img`
+width: 50%;
+height: auto;
+padding 2rem;`
+const Form = styled.form`
+display: flex;
+flex-flow: column nowrap;
+align-items: center;
+justify-content: space-around;
+height: 25vh;`
+const Button = styled.button`
+background-color: white;
+:active{
+background-color: #A0C9F2;
+}
+color: #2768B3;
+padding: 1rem;
+text-align: center;
+display: inline-block;
+font: bold 4vw Open Sans;
+font-size: 4vw;
+border-radius: 0.5rem;
+margin: 2rem;`
 
 export default () => {
-    const classes = useStyles();
 
     const [email, setEmail] = useState(null);
     const history = useHistory();
@@ -41,8 +75,11 @@ export default () => {
     }
 
 
-    return (<>
-        <form className={classes.root} noValidate autoComplete="on" action="javascript:void(0);" onSubmit={async () => {
+    return (
+        <Background>
+        <Image src={Heart} />
+        <Title>Enter your email</Title>
+        <Form noValidate autoComplete="on" action="javascript:void(0);" onSubmit={async () => {
             // history.push("/signup-bio");
             const res = await checkemail();
             // console.log("REEEEEE: " + res.status);status
@@ -58,14 +95,13 @@ export default () => {
                 console.log("I got 204")
             }
         }}>
-            <TextField
+            <Text
                 // value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 id="standard-basic"
                 label="email" />
-            <button type="submit">Verify</button>
-        </form>
-        <div>{email}</div>
-    </>
+            <Button type="submit">Verify</Button>
+        </Form>
+    </Background>
     );
 }
