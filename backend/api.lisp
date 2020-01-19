4eeb1ecb956bd88b85851ef445723d096d96ba0a
Peter Elliott <pelliott@ualberta.ca>
2020-01-19 01:14:44 -0700

    added cors middleware

--- backend/api.lisp
+++ backend/api.lisp
@@ -13,8 +13,18 @@
   (and (>= (length str) (length start))
        (string= start (subseq str 0 (length start)))))
 
+(defun cors (app)
+  (lambda (env)
+    (let ((r (funcall app env)))
+      (setf (cadr r) (append '(:access-control-allow-methods "POST"
+                               :access-control-allow-origin "*"
+                               :access-control-allow-headers "content-type")
+                             (cadr r)))
+      r)))
+
 (defvar *static-app*
   (lack:builder
+   #'cors
    (lambda (app)
      (let ((static (funcall lack.middleware.static:*lack-middleware-static*
                             app
